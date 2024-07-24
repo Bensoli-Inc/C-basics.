5a196be68810c6deb562e3e87bc44e53ac6e702d
Bensoli <benswambua96@gmail.com>
2024-07-24 06:03:51 +0300

    arrays, madlibgame

--- userInput.c
+++ userInput.c
@@ -4,17 +4,23 @@
 //HOW TO TAKE INFO FROM USER AND STORE inside a variable to use inside a program
 
 int main() {
-    // int age; //declare but dont give value
-    // printf("Enter your age: \n");
-    // scanf("%d", &age); // (pointer) allow get info from user. uSE ARMPERSAND  before the variable name
+    int age; //declare but dont give value
+    printf("Enter your age: \n");
+    scanf("%d", &age); // (pointer) allow get info from user. uSE ARMPERSAND  before the variable name
   
-    // printf("you are %d years old\n", age);
-    // return 0; 
+    printf("you are %d years old\n", age);
+    return 0; 
 
 
     char name[20]; //specify max no of chacters
     printf("Enter your name: \n");
     scanf("%s", name);
+    printf("your name is %s", name);
+
+    //FGETS to get string with spaces
+     char name[50]; 
+    printf("Enter your name: \n");
+    fgets(name, 50, stdin); //start with variable name, then max no of xters, then standard input
     printf("your name is %s", name);
 }
 
